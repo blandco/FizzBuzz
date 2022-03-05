4153028abf94b89a566e083d79b2bf27c87acc17
Joel Bland <jbland@gmail.com>
2022-03-04 23:23:15 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# FizzBuzz
+
+This program checks if an integer is Fizz, Buzz, or FizzBuzz.
+
+## Usage
+
+Enter an integer value to check.
+
+- Integer divisible by 3 == Fizz
+- Integer divisible by 5 == Buzz
+- Integer divisible by 3 and 5 == FizzBuzz
+
+
---
+++ src/FizzBuzz.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+class FizzBuzz
+{
+  public static void main(String[] args) 
+  {
+
+    // The number to check for FizzBuzz.
+    int numCheck = 0;
+
+    // Create scanner object for input.
+    Scanner keyboard = new Scanner(System.in);
+
+    // Display the program title.
+    generateTitle();
+    
+    // Display instructions.
+    System.out.println("Numbers divisble by 3 are Fizz.");
+    System.out.println("Numbers divisble by 5 are Buzz.");
+    System.out.println("Numbers divisble by 3 and 5 are FizzBuzz.");
+    System.out.printf("Enter a number to check it's value: ");
+
+    // Get the number from the Scanner object.
+    numCheck = keyboard.nextInt();
+
+    // Call the isBuzz method to check 
+    // for Fizz, Buzz, or FizzBuzz and
+    // assign the results to returnValue.
+    int returnValue = isBuzz(numCheck);
+    
+    // Close the Scanner object.
+    keyboard.close();
+
+    // Determine which output to display based on the
+    // returnValue from the isBuzz method.
+    if (returnValue == -3)
+    {
+        System.out.println(numCheck + " divided by 3 equals " + numCheck / 3);
+        System.out.println(numCheck + " divided by 5 equals " + numCheck / 5);
+        System.out.println(numCheck + " is FizzBuzz.");
+    }
+    else if (returnValue == -1)
+    {
+        System.out.println(numCheck + " divided by 3 equals " + numCheck / 3);
+        System.out.println(numCheck + " is Fizz.");
+    }
+    else if (returnValue == -2)
+    {
+        System.out.println(numCheck + " divided by 5 equals " + numCheck / 5);
+        System.out.println(numCheck + " is Buzz.");
+    }
+    else 
+    {
+        System.out.println(numCheck + " is not Fizz, Buzz, or FizzBuzz!");
+    }
+  }
+
+    /**
+      The isBuzz method determines if the integer is divisible by 3, 5, or both.
+      @param i An integer.
+      @return -1=Fizz, -2=Buzz, -3=FizzBuzz, or int.
+    */
+    public static int isBuzz(int i)
+    {
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        return -3; // 333=FizzBuzz
+      }
+      else if (i % 3 == 0)
+      {
+        return -1; // 111=Fizz
+      }
+      else if (i % 5 == 0)
+      {
+				return -2; // 222=Buzz
+			}
+      else
+      {
+        return i;
+      }
+    
+  }
+
+  /**
+      The generateTitle method outputs a Title header.
+    */
+    public static void generateTitle()
+    {
+    String tableCharacter = "/";
+    String headerText = "FIZZ BUZZ CHECKER";
+    int headerLength = headerText.length();
+    final int COLUMNS = headerLength + 6;
+    final int ROWS = 1;
+
+    // Print the first half of the table. 
+    for (int row = 0; row < ROWS; row++)
+    {
+        for (int col = 0; col < COLUMNS; col++)
+        {
+          System.out.printf(tableCharacter);
+        }
+    System.out.println();
+    }
+
+    //Jump out and print the headerText padded with 2 tableCharacters on each side of the text.
+    System.out.printf(tableCharacter + tableCharacter + " " + headerText + " " + tableCharacter + tableCharacter + "\n");
+
+    // Print the last half of the table. 
+    for (int row = 0; row < ROWS; row++)
+    {
+        for (int col = 0; col < COLUMNS; col++)
+        {
+          System.out.printf(tableCharacter);
+        }
+    System.out.println();
+    }
+
+  }
+    
+}
